d39c0d5fe0ad2d1fe3d59cb1bd56c2c5eeceecb8
oliverablagojevic <olja_blagojevic@hotmail.com>
2020-08-24 00:59:08 +0200

    Update ApplicationYmlGenerator.java

--- PluginDevelopment/src/springplugin/generator/ApplicationYmlGenerator.java
+++ PluginDevelopment/src/springplugin/generator/ApplicationYmlGenerator.java
@@ -2,9 +2,15 @@ package springplugin.generator;
 
 import springplugin.configuration.ApplicationConfiguration;
 import springplugin.generator.fmmodel.FMEntity;
+import springplugin.generator.fmmodel.FMModel;
 import springplugin.generator.options.GeneratorOptions;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.swing.JOptionPane;
 
 public class ApplicationYmlGenerator extends BasicGenerator {
 
@@ -13,13 +19,25 @@ public class ApplicationYmlGenerator extends BasicGenerator {
         super(generatorOptions);
     }
 
-    /* 
+/*  
     @Override
-    protected void populateContext(entity, Map<String, Object> context) {
+    public void generate() {
+        try {
+            super.generate();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+    protected abstract void populateContext(FMEntity cl, Map<String, Object> context)
+            throws TemplateException;
+            
+     protected void populateContext(FMentity, Map<String, Object> context) {
     	context.put("projectName", entity.getProjectName());
 		context.put("port", entity.getPort());
 		context.put("databaseUrl", entity.getDatabaseUrl());
 		context.put("databaseUsername", entity.getDatabaseUsername());
 		context.put("databasePassword", entity.getDatabasePassword());
-    }*/
+    }
+}*/
+
 }
